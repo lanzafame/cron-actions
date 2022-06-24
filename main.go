09f176b16721f0199c241c3c104d29fbb8d9e72e
lanzafame <adrianlanzafame92@gmail.com>
2022-06-24 13:47:07 +1000

    add nft.storage api request for static nft

--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+const imageCID = "bafybeibtbsrwnifl5drdzmu2v5fgcyq4othrlx3h3shcvua2om25gnfmvy"
 
 func main() {
 	fmt.Println("hello github actions")
+
+	//TODO create histogram metric
+
+	client := &http.Client{}
+
+	url := strings.Join("https://api.nft.storage/", imageCID)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Printf("err: %w\n", err)
+		return
+	}
+
+	tok := os.Getenv("NFT_STORAGE_TOKEN")
+	req.Header.Add("Authorization", tok)
+
+	//TODO start timer
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("err: %w\n", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("err: %w\n", err)
+		return
+	}
+
+	fmt.Println(body)
+
+	//TODO finish timer
+
+	//TODO push metric
+
 }
